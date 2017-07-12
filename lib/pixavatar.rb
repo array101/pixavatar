33433c6674bcea0a5d538a4a7256c9553894c23e
Sandeep Acharya <acharya877@gmail.com>
2017-07-12 20:07:58 +0545

    hash input string with md5 and store in a 16 item array

--- lib/pixavatar.rb
+++ lib/pixavatar.rb
@@ -1,5 +1,22 @@
+require 'digest/md5'
+
 class Pixavatar
-	def self.hi
-		puts 'Hello World !!'
+	attr_accessor :avatar
+
+	def initialize(input_term) 
+		@term = input_term
+		@avatar = {
+				hex: hashed_input
+			}
+	end
+	
+	# convert input string to MD5 hex digest and return decimal array of the digest
+	def hashed_input
+		md5_hex = Digest::MD5.hexdigest(@term)
+		md5_hex.scan(/../).map { |s| s.to_i(16) }
 	end
 end
+
+tmp = Pixavatar.new('banana')
+p tmp.inspect
+
